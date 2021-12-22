89486b386ef4ecc9e4f068cea613a62b20ab3810
Chetan Kumar N <chetankumar.nv@gmail.com>
2021-12-22 13:48:49 +0530

    prop

---
+++ src/Components/Villas/slider/AdarshSlider.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Carousel} from 'react-bootstrap'
+import Image1 from '../../../Assets/images/projects/adrash-palm-acres/slider/1.png'
+
+export default function AdarshSlider() {
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={Image1}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Adarsh Palm Acres in Bagalur, Bangalore | Reviews | Group Buy | Price</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
